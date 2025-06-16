06379fcf69585c252b151b1c291e2cc97b3bc90d
erfchiujhf <sofianovikova295@gmail.com>
2025-06-16 20:55:43 +0300

    sdelali prototip
    dobavili raketki, dobavili myachik, dobavili proigrish

--- main.py
+++ main.py
@@ -36,12 +36,38 @@ display.set_caption('пинг-понг')
 run = True
 finish = False
 fps = 60
+speed_x = 3
+speed_y = 3
 clock = time.Clock()
+racket1 = Player('racket.png', 30, 200, 50, 150, 4)
+racket2 = Player('racket.png', 520, 200, 50, 150, 4)
+ball = GameSprite('La-Vaca-Saturno-Saturnita-PNG.png', 200, 200, 50, 50, 4)
+font.init()
+font1 = font.SysFont('Arial', 100)
+loser1 = font1.render('1 lose', True, (74, 23, 123))
+loser2 = font1.render('2 lose', True, (56, 1, 30))
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
     if not finish:
         window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if ball.rect.y > height - 50 or ball.rect.y < 0:
+            speed_y *= -1
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(loser1, (200, 200))
+        if ball.rect.x > width:
+            finish = True
+            window.blit(loser2, (200, 200))
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
     display.update()
     clock.tick(fps)
